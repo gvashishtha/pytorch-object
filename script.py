eeb837f88c93cfcdf6f391cd3730ae416275ff7c
Gopal K. Vashishtha <gopalv@microsoft.com>
2020-03-03 16:26:54 -0800

    added headers, refactored, GPU not working

--- script.py
+++ script.py
@@ -8,30 +8,9 @@ import utils
 from azureml.core import Dataset, Run
 from data import PennFudanDataset
 from engine import train_one_epoch, evaluate
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+from model import get_instance_segmentation_model
 
 NUM_CLASSES = 2
-
-
-def get_instance_segmentation_model(num_classes):
-    # load an instance segmentation model pre-trained on COCO
-    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
-
-    # get the number of input features for the classifier
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    # replace the pre-trained head with a new one
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-    # now get the number of input features for the mask classifier
-    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-    hidden_layer = 256
-    # and replace the mask predictor with a new one
-    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
-                                                       hidden_layer,
-                                                       num_classes)
-
-    return model
 
 
 def get_transform(train):
@@ -83,9 +62,11 @@ def main():
         collate_fn=utils.collate_fn)
 
     if torch.cuda.is_available():
+        print('Using GPU')
         device = torch.device('cuda')
     else:
-        torch.device('cpu')
+        print('Using CPU')
+        device = torch.device('cpu')
 
     # our dataset has two classes only - background and person
     num_classes = NUM_CLASSES
